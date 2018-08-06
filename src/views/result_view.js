21e7f13ce97117988e1b60827821d51644a2b5df
Gregor Cox <gregorcox@gmail.com>
2018-08-06 19:37:00 +0100

    adds result view

---
+++ src/views/result_view.js
@@ -0,0 +1,21 @@
+const PubSub = require('../../helpers/pub_sub.js');
+
+const ResultView = function () {
+
+};
+
+ResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('WordChecker:result', (evt) => {
+    const result = evt.detail;
+    this.updateView(result);
+  })
+};
+
+ResultView.prototype.updateView = function (result) {
+  const resultElement = document.querySelector('#result');
+  resultElement.textContent = `There are ${result} words`;
+  }
+
+};
+
+module.exports = ResultView;
